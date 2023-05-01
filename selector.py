4672ec4c1db6db75c480bcc95cea06230fc5ad29
sh9634 <sanghun9634@gmail.com>
2023-05-01 14:24:15 +0900

    feat: Add Define random_select()

--- selector.py
+++ selector.py
@@ -1,5 +1,13 @@
-import sys
+import random
 
-print(sys.version)
+def random_select():
+    lotto = []
+    for x in range(6):
+        lotto.append(random.randint(1,45))
+    print(lotto)
 
+
+if __name__ =="__main__":
+    
+    random_select()
 
