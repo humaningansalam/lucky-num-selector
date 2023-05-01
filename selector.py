fd6c0ccaf3f9d557ff28f75c613ea24eb9fd6b2d
sh9634 <sanghun9634@gmail.com>
2023-05-01 14:40:47 +0900

    refactor: random_select to get_lucky_num

--- selector.py
+++ selector.py
@@ -1,13 +1,16 @@
 import random
 
-def random_select():
-    lotto = []
-    for x in range(6):
-        lotto.append(random.randint(1,45))
-    print(lotto)
+def get_lucky_num(attemp):
+    
+
+    for y in range(attemp):
+        lotto = []
+        for x in range(6):
+            lotto.append(random.randint(1,45))
+        print(lotto)
 
 
 if __name__ =="__main__":
-    
-    random_select()
+    attemp = int(input("몇번 시도하시겠습니까?"))
+    random_select(attemp)
 
